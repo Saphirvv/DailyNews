72fa2cfa29e96ba43bf99ddd547443fdbff89ee1
Saphirvv <919284146@qq.com>
2016-04-13 14:06:46 +0800

    util improving

--- app/src/main/java/com/saphir/test/dailynews/utils/FormatUtil.java
+++ app/src/main/java/com/saphir/test/dailynews/utils/FormatUtil.java
@@ -1,5 +1,8 @@
 package com.saphir.test.dailynews.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Saphir
  * on 2016/4/11.
@@ -18,5 +21,29 @@ public class FormatUtil {
             return s.substring(0, length) + "...";
         }
         return s;
+    }
+
+    /**
+     * 规范句首字母大写
+     */
+    //确认字符是否为英文字母
+    private static Pattern sWordPattern = Pattern.compile("\\w+");
+
+    public static String capitalizeSentence(final String sentence) {
+        String result = sentence;
+        Matcher matcher = sWordPattern.matcher(result);
+        while (matcher.find()) {
+            String word = matcher.group();
+            //将首字母大写的词，替换原来匹配出来的英文词
+            result = result.replace(matcher.group(), capitalize(word));
+        }
+        return result;
+    }
+
+    public static String capitalize(final String word) {
+        if (word.length() > 1) {
+            return String.valueOf(word.charAt(0)).toUpperCase() + word.substring(1);
+        }
+        return word;
     }
 }
