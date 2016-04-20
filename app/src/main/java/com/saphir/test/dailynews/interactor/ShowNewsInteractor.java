e94cd9ef874c374121fc38d03307b1b5f2c916af
Saphirvv <919284146@qq.com>
2016-04-20 09:51:31 +0800

    delete useless

--- app/src/main/java/com/saphir/test/dailynews/interactor/ShowNewsInteractor.java
+++
@@ -1,9 +0,0 @@
-package com.saphir.test.dailynews.interactor;
-
-/**
- * 详情页 - 显示新闻，读取实体类
- * Created by Saphir
- * on 2016/4/8.
- */
-public interface ShowNewsInteractor {
-}
--- app/src/main/java/com/saphir/test/dailynews/interactor/UploadDataInteractorImpl.java
+++
@@ -1,9 +0,0 @@
-package com.saphir.test.dailynews.interactor;
-
-/**
- * 更新的数据上传至云端服务器 的 实现
- * Created by Saphir
- * on 2016/4/6.
- */
-public class UploadDataInteractorImpl implements UploadDataInteractor {
-}
